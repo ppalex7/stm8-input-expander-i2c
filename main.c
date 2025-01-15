9d23bc88e66c1db90af995d5673894c6ec706e60
Aleksandr Duplishchev <2ppalex@gmail.com>
2025-01-15 09:42:22 +0300

    configure IO for extib

--- main.c
+++ main.c
@@ -2,5 +2,16 @@
 
 void main(void)
 {
+	// set PORT B external interrupt sensivity to "rising and falling edge"
+	EXTI->CR3 &= (uint8_t)(~EXTI_CR3_PBIS);
+	EXTI->CR3 |= (0b11 << 0);
+	// PB[7:4] are used for EXTIB interrupt generation
+	EXTI->CONF1 |= (0b1 << 1);
+
+	// enable interrupts for PB4-PB7
+	GPIOB->CR2 |= 0b11110000;
+
+	enableInterrupts();
+
 	while (1);
 }
