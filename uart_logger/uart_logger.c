b1ce0b7ea4f078d63e6c506711ffe6c7e135b99c
Aleksandr Duplishchev <2ppalex@gmail.com>
2025-03-16 12:12:55 +0300

    uart async logger

---
+++ uart_logger/uart_logger.c
@@ -0,0 +1,108 @@
+#include "uart_logger.h"
+#include "stm8l15x.h"
+#include <stdio.h>
+
+#define BUFFER_MESSAGES_COUNT 5u
+#define USART_DR (USART1_BASE + 0x01)
+
+@near unsigned char g_uart_tx_buffer[128];
+static volatile uint8_t length = 0;
+// 2-byte index produces less code for array addressing;
+static volatile uint16_t idx = 0;
+static unsigned char drops = 0;
+static volatile char *msg_format[BUFFER_MESSAGES_COUNT] = {0};
+static volatile short msg_arg[BUFFER_MESSAGES_COUNT];
+
+void configure_logger_peripheral(unsigned short clock_divider)
+{
+    // Feed clock to USART1
+    CLK->PCKENR1 |= CLK_PCKENR1_USART1;
+
+    USART1->BRR2 = (uint8_t)(clock_divider & 0x0F);
+    USART1->BRR1 = (uint8_t)(clock_divider >> 4);
+    // Enable transmitter
+    USART1->CR2 |= USART_CR2_TEN;
+    // Enable DMA mode for transmission
+    USART1->CR5 |= USART_CR5_DMAT;
+
+    // Feed clock to DMA1
+    CLK->PCKENR2 |= CLK_PCKENR2_DMA1;
+    // set destination peripheral address
+    DMA1_Channel1->CPARH = (uint8_t)(USART_DR >> 8);
+    DMA1_Channel1->CPARL = (uint8_t)(USART_DR);
+    // set channe priority to high
+    DMA1_Channel1->CSPR |= (uint8_t)(0b10 << 4);
+    // set memory increment mode and data transfer direction from memory to peripheral
+    DMA1_Channel1->CCR |= (DMA_CCR_IDM | DMA_CCR_DTD | DMA_CCR_TCIE);
+    // DMA global enable
+    DMA1->GCSR |= DMA_GCSR_GE;
+}
+
+void logf(@near char *fmt, uint16_t arg)
+{
+    if (idx > BUFFER_MESSAGES_COUNT - 1)
+    {
+        // buffer is full, drop message
+        drops++;
+        return;
+    }
+
+    msg_format[idx] = fmt;
+    msg_arg[idx] = arg;
+    idx++;
+}
+
+void on_dma_log_transfer_complete(void)
+{
+    uint8_t i;
+    uint8_t max_idx;
+
+    if (DMA1_Channel1->CSPR & DMA_CSPR_TCIF)
+    {
+        max_idx = (uint8_t)((idx > BUFFER_MESSAGES_COUNT - 1)
+                                ? BUFFER_MESSAGES_COUNT - 1
+                                : idx);
+        if (idx == 1)
+        {
+            msg_format[0] = 0;
+            msg_arg[0] = 0;
+        }
+        else
+        {
+            for (i = 0; i < max_idx; i++)
+            {
+                msg_format[i] = msg_format[i + 1];
+                msg_arg[i] = msg_arg[i + 1];
+            }
+        }
+        idx--;
+        length = 0;
+
+        DMA1_Channel1->CSPR &= (uint8_t)~DMA_CSPR_TCIF;
+    }
+}
+
+void process_buffered_logs(void)
+{
+    // return if busy or has no messages
+    if (length || !msg_format[0])
+    {
+        return;
+    }
+
+    length = (uint8_t)sprintf(g_uart_tx_buffer, msg_format[0], msg_arg[0]);
+
+    // disable channel
+    DMA1_Channel1->CCR &= (uint8_t)~DMA_CCR_CE;
+
+    // set source address
+    DMA1_Channel1->CM0ARH = (uint8_t)(((uint16_t)g_uart_tx_buffer) >> (uint8_t)8);
+    DMA1_Channel1->CM0ARL = (uint8_t)(((uint16_t)g_uart_tx_buffer));
+
+    DMA1_Channel1->CNBTR = length;
+
+    // trigger UART
+    USART1->SR &= (uint8_t)(~USART_SR_TC);
+    // enable channel
+    DMA1_Channel1->CCR |= DMA_CCR_CE;
+}
---
+++ uart_logger/uart_logger.h
@@ -0,0 +1,10 @@
+#ifndef __UART_LOGGER_H
+#define __UART_LOGGER_H
+
+#define log(a) logf(a, 0)
+void logf(@near char* fmt, unsigned short arg);
+void configure_logger_peripheral(unsigned short clock_divider);
+void on_dma_log_transfer_complete(void);
+void process_buffered_logs(void);
+
+#endif
